73e797f009c36e3c138251d92fe05e0c5b3a1b36
Yihao Jia <jiayihao@users.noreply.github.com>
2022-04-18 22:22:04 +0800

    Create selectionSort.py

---
+++ selectionSort.py
@@ -0,0 +1,9 @@
+def selectSort(self, nums: List[int]) -> List[int]:
+  length = len(nums)
+  for i in range(length):
+	  minIndex = i
+	  for j in range(i + 1, length):
+		  if nums[j] < nums[minIndex]:
+			  minIndex = j
+	  nums[minIndex], nums[i] = nums[i], nums[minIndex]
+  return nums
